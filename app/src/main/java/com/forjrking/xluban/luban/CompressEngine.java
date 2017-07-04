cfd24c926110f4bf7baa9ef25f3dbe6e60b487a4
forJrking <897172650@qq.com>
2017-07-04 14:43:04 +0800

    添加缩放算法压缩
    添加缩放算法压缩

--- app/src/main/java/com/forjrking/xluban/luban/CompressEngine.java
+++ app/src/main/java/com/forjrking/xluban/luban/CompressEngine.java
@@ -1,9 +1,13 @@
-package com.forjrking.xluban.luban;
+package com.imaginer.yunjicore.image;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -21,22 +25,28 @@ import java.io.IOException;
 public class CompressEngine {
 
     private ExifInterface srcExif;
-    private File srcImg;
-    private File tagImg;
+    private File srcFile;
+    private File resFile;
 
-    private ByteArrayOutputStream mStream;
-    private Bitmap.CompressFormat compressFormat;
-    private Bitmap.Config compressConfig;
+    private ByteArrayOutputStream stream;
 
-    CompressEngine(File srcImg, File tagImg, Bitmap.CompressFormat compressFormat, Bitmap.Config compressConfig) throws IOException {
-        if (isJpeg(srcImg)) {
-            this.srcExif = new ExifInterface(srcImg.getAbsolutePath());
+    private static float REFERENCE_WIDTH = 1080f;
+    private static float SCALE_REFERENCE_WIDTH = 1280f;
+    private static float LIMITED_WIDTH = 1000f;
+    private static float MIN_WIDTH = 640f;
+    //常用压缩比
+    private static int DEFAULT_QUALITY = 66;
+    private static int DEFAULT_LOW_QUALITY = 60;
+    private static int DEFAULT_HEIGHT_QUALITY = 82;
+    private static int DEFAULT_X_HEIGHT_QUALITY = 88;
+    private static int DEFAULT_XX_HEIGHT_QUALITY = 94;
+
+    CompressEngine(File srcFile, File resFile) throws IOException {
+        this.srcFile = srcFile;
+        this.resFile = resFile;
+        if (isJpeg(this.srcFile)) {
+            this.srcExif = new ExifInterface(this.srcFile.getAbsolutePath());
         }
-        this.srcImg = srcImg;
-        this.tagImg = tagImg;
-        this.compressFormat = compressFormat;
-        this.compressConfig = compressConfig;
-
     }
 
     private boolean isJpeg(File photo) {
@@ -45,7 +55,108 @@ public class CompressEngine {
     }
 
     /**
-     * @return 采样率  核心算法
+     * 判断内存是否足够 32位每个像素占用4字节
+     */
+    private boolean hasEnoughMemory(int width, int height, boolean isAlpha32) {
+        Runtime runtime = Runtime.getRuntime();
+        long free = runtime.maxMemory() - runtime.totalMemory() + runtime.freeMemory();
+        int allocation = (width * height) << (isAlpha32 ? 2 : 1);
+        Log.d("Luban", "free : " + (free >> 20) + "MB, need : " + (allocation >> 20) + "MB");
+        return allocation < free;
+    }
+
+    File compress(boolean compress4Sample, Bitmap.CompressFormat compressFormat, Bitmap.Config compressConfig) throws IOException {
+        return customCompress(-1, compress4Sample, 50, compressFormat, compressConfig);
+    }
+
+    public File customCompress(int maxSize, boolean compress4Sample, int quality, Bitmap.CompressFormat compressFormat, Bitmap.Config compressConfig) throws IOException {
+        long rqSize = srcFile.length() / 1024;
+//      不压缩
+        if (maxSize >= rqSize) {
+            return srcFile;
+        }
+        rqSize = maxSize;
+
+        //解析Bitmap
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        //不加载进内存
+        options.inJustDecodeBounds = true;
+        //默认采样率 采样率是2的次幂
+        options.inSampleSize = 1;
+        //不加载进内存解析一次 获取宽高
+        BitmapFactory.decodeFile(srcFile.getAbsolutePath(), options);
+        //解析出宽高
+        int width = options.outWidth;
+        int height = options.outHeight;
+        //重新 计算采样率 来压缩
+        if (compress4Sample) {
+            options.inSampleSize = computeSize(width, height);
+        }
+        // 指定图片 ARGB 或者RGB
+        options.inPreferredConfig = compressConfig;
+        //内存不足情况
+        boolean isAlpha = compressConfig == Bitmap.Config.ARGB_8888;
+
+        if (!hasEnoughMemory(width / options.inSampleSize, height / options.inSampleSize, isAlpha)) {
+            //TODO 内存不足使用
+            options.inPreferredConfig = compressFormat == Bitmap.CompressFormat.PNG ?
+                    Bitmap.Config.ARGB_4444 : Bitmap.Config.RGB_565;
+            //减低像素喂 减低内存
+            if (!hasEnoughMemory(width / options.inSampleSize, height / options.inSampleSize, false)) {
+                //并且重新计算采样率
+                options.inSampleSize = computeSize(width, height);
+                if (!hasEnoughMemory(width / options.inSampleSize, height / options.inSampleSize, false)) {
+                    throw new RuntimeException("image length is too large");
+                }
+            }
+        }
+        //加载入内存中
+        options.inJustDecodeBounds = false;
+        //此处OOM
+        Bitmap bitmap = BitmapFactory.decodeFile(srcFile.getAbsolutePath(), options);
+        //解析出错
+        if (bitmap == null) {
+            return srcFile;
+        }
+        //精准缩放压缩
+        if (!compress4Sample) {
+            float scale = calculateScaleSize(width, height);
+            Log.d("Luban", "scale : " + scale);
+            bitmap = transformBitmap(bitmap, scale);
+        } else {
+            //旋转默认角度
+            bitmap = transformBitmap(bitmap, 1f);
+        }
+        // 获取解析流
+        if (stream == null) {
+            stream = new ByteArrayOutputStream();
+        } else {
+            stream.reset();
+        }
+        //压缩开始
+        bitmap.compress(compressFormat, quality, stream);
+        //无损不支持压缩
+        if (compressFormat != Bitmap.CompressFormat.PNG && rqSize > 0) {
+            //耗时由此处触发
+            while (stream.size() / 1024 > rqSize && quality > 6) {
+                stream.reset();
+                quality -= 6;
+                bitmap.compress(compressFormat, quality, stream);
+            }
+        }
+        //标记释放
+        bitmap.recycle();
+        //输出文件
+        FileOutputStream fos = new FileOutputStream(resFile);
+        stream.writeTo(fos);
+        fos.flush();
+        fos.close();
+        stream.close();
+        return resFile;
+    }
+
+    /**
+     * 采样率  核心算法
      */
     private int computeSize(int width, int height) {
         int mSampleSize;
@@ -77,142 +188,89 @@ public class CompressEngine {
         return mSampleSize;
     }
 
-    private Bitmap rotatingImage(Bitmap bitmap) {
-        if (srcExif == null) return bitmap;
-
-        Matrix matrix = new Matrix();
-        int angle = 0;
-        int orientation = srcExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-        switch (orientation) {
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                angle = 90;
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                angle = 180;
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                angle = 270;
-                break;
-        }
-
-        matrix.postRotate(angle);
-
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-    }
-
-    File compress() throws IOException {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        options.inSampleSize = 1;
-
-        BitmapFactory.decodeFile(srcImg.getAbsolutePath(), options);
-        //计算采样率
-        options.inSampleSize = computeSize(options.outWidth, options.outHeight);
-        options.inPreferredConfig = compressConfig;
-        options.inJustDecodeBounds = false;
-        Bitmap tagBitmap = BitmapFactory.decodeFile(srcImg.getAbsolutePath(), options);
-
-        if (mStream == null) {
-            mStream = new ByteArrayOutputStream();
+    /**
+     * 精确缩放
+     */
+    private float calculateScaleSize(int width, int height) {
+        float scale = 1f;
+        int max = Math.max(width, height);
+        int min = Math.min(width, height);
+        float ratio = min / (max * 1f);
+        if (ratio >= 0.5f) {
+            if (max > SCALE_REFERENCE_WIDTH) scale = SCALE_REFERENCE_WIDTH / (max * 1f);
         } else {
-            mStream.reset();
-        }
-
-        tagBitmap = rotatingImage(tagBitmap);
-        tagBitmap.compress(compressFormat, 50, mStream);
-        tagBitmap.recycle();
-
-        FileOutputStream fos = new FileOutputStream(tagImg);
-        fos.write(mStream.toByteArray());
-        fos.flush();
-        fos.close();
-        mStream.close();
-
-        return tagImg;
-    }
-
-    public File customCompress(int maxSize, int maxWidth, int maxHeight) throws IOException {
-        long rqSize = srcImg.length() / 1024;
-//         不压缩
-        if (maxSize >= rqSize) {
-            return srcImg;
-        }
-        rqSize = maxSize;
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true; //不加载进内存
-        options.inSampleSize = 1;
-        BitmapFactory.decodeFile(srcImg.getAbsolutePath(), options);
-        // //计算采样率 值再次解析图片
-        if (maxWidth == 0 && maxHeight == 0) {
-            options.inSampleSize = computeSize(options.outWidth, options.outHeight);
-        } else {
-            options.inSampleSize = computeSampleSize(options.outWidth, options.outHeight, maxWidth, maxHeight);
-        }
-        options.inPreferredConfig = compressConfig;
-        options.inJustDecodeBounds = false;
-        //此处OOM
-        Bitmap bitmap = BitmapFactory.decodeFile(srcImg.getAbsolutePath(), options);
-
-        if (mStream == null) {
-            mStream = new ByteArrayOutputStream();
-        } else {
-            mStream.reset();
-        }
-        bitmap = rotatingImage(bitmap);
-
-        int quality = 100;
-
-        bitmap.compress(compressFormat, quality, mStream);
-
-        if (compressFormat != Bitmap.CompressFormat.PNG) {
-//            PNG不支持
-            while (mStream.size() / 1024 > rqSize && quality > 6) {
-                mStream.reset();
-                quality -= 6;
-                bitmap.compress(compressFormat, quality, mStream);
+            int multiple = max / min;
+            if (multiple < 10) {
+                if (min > LIMITED_WIDTH && (1f - (ratio / 2f)) * min > LIMITED_WIDTH) {
+                    scale = 1f - (ratio / 2f);
+                }
+            } else {
+                int arg = (int) Math.pow(multiple, 2);
+                scale = 1f - (arg / LIMITED_WIDTH) + (multiple > 10 ? 0.01f : 0.03f);
+                if (min * scale < MIN_WIDTH) {
+                    scale = 1f;
+                }
             }
         }
-
-        bitmap.recycle();
-        FileOutputStream fos = new FileOutputStream(tagImg);
-        mStream.writeTo(fos);
-        fos.flush();
-        fos.close();
-        mStream.close();
-
-        return tagImg;
+        return scale;
     }
 
     /**
-     * 计算目标宽度，目标高度，inSampleSize
+     * 缩放 旋转bitmap scale =1f不缩放
      */
-    private static int computeSampleSize(int outWidth, int outHeight, int reqWidth, int reqHeight) {
-
-        reqWidth = getResizedDimension(reqWidth, reqHeight, outWidth, outHeight);
-        reqHeight = getResizedDimension(reqHeight, reqWidth, outHeight, outWidth);
-        // 根据现在得到计算inSampleSize
-        return calculateBestInSampleSize(outWidth, outHeight, reqWidth, reqHeight);
-    }
-
-    private static int getResizedDimension(int maxPrimary, int maxSecondary, int actualPrimary, int actualSecondary) {
-        double ratio = (double) actualSecondary / (double) actualPrimary;
-        int resized = maxPrimary;
-        if (resized * ratio > maxSecondary) {
-            resized = (int) (maxSecondary / ratio);
-        }
-        return resized;
-    }
-
-    private static int calculateBestInSampleSize(int actualWidth, int actualHeight, int desiredWidth, int desiredHeight) {
-        double wr = (double) actualWidth / desiredWidth;
-        double hr = (double) actualHeight / desiredHeight;
-        double ratio = Math.min(wr, hr);
-        float inSampleSize = 1.0f;
-        while ((inSampleSize * 2) <= ratio) {
-            inSampleSize *= 2;
+    private Bitmap transformBitmap(Bitmap bitmap, float scale) {
+        //过滤无用的压缩
+        if (srcExif == null && scale == 1f) return bitmap;
+        Matrix matrix = new Matrix();
+        if (srcExif != null) {
+            int angle = 0;
+            int orientation = srcExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    angle = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    angle = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    angle = 270;
+                    break;
+            }
+            matrix.postRotate(angle);
         }
 
-        return (int) inSampleSize;
+        if (scale != 1f) {
+            matrix.setScale(scale, scale);
+        }
+        try {
+            Bitmap converted = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            if (!bitmap.sameAs(converted)) {
+                bitmap = converted;
+            }
+        } catch (OutOfMemoryError error) {
+            System.gc();
+            System.runFinalization();
+        }
+        return bitmap;
+    }
+
+    /**
+     * @return 计算合适的压缩率
+     */
+    public static int calculateQuality(Context context) {
+        DisplayMetrics dm = new DisplayMetrics();
+        ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(dm);
+        float density = dm.density;
+        if (density > 3f) {
+            return DEFAULT_LOW_QUALITY;
+        } else if (density > 2.5f && density <= 3f) {
+            return DEFAULT_QUALITY;
+        } else if (density > 2f && density <= 2.5f) {
+            return DEFAULT_HEIGHT_QUALITY;
+        } else if (density > 1.5f && density <= 2f) {
+            return DEFAULT_X_HEIGHT_QUALITY;
+        } else {
+            return DEFAULT_XX_HEIGHT_QUALITY;
+        }
     }
 }
--- app/src/main/java/com/forjrking/xluban/luban/Luban2.java
+++ app/src/main/java/com/forjrking/xluban/luban/Luban2.java
@@ -1,4 +1,4 @@
-package com.forjrking.xluban.luban;
+package com.imaginer.yunjicore.image;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 
 /**
  * @创建者 froJrking
@@ -35,12 +36,18 @@ public class Luban2 {
 
     private Bitmap.CompressFormat compressFormat = Bitmap.CompressFormat.JPEG;
 
-    private Bitmap.Config compressConfig = Bitmap.Config.ARGB_8888; //主要目的减少oom
+    private Bitmap.Config compressConfig = Bitmap.Config.ARGB_8888; //566可以减少oom
+
+    private int bestQuality = 100;
+
+    private boolean compress4Sample = true; //使用采样率压缩
 
     private Luban2(Context context) {
         mImageCacheDir = getImageCacheDir(context);
         if (executor == null)
             executor = new AsyncExecutor();
+        //计算大致系数
+        bestQuality = CompressEngine.calculateQuality(context);
     }
 
     public static Luban2 with(Context context) {
@@ -85,8 +92,17 @@ public class Luban2 {
      * @param compressFormat 图片格式
      * @return
      */
-    public Luban2 setFormat(Bitmap.CompressFormat compressFormat) {
+    public Luban2 format(Bitmap.CompressFormat compressFormat) {
         this.compressFormat = compressFormat;
+        return this;
+    }
+
+    /**
+     * @param compress4Sample 图片格式
+     * @return
+     */
+    public Luban2 compressType(boolean compress4Sample) {
+        this.compress4Sample = compress4Sample;
         return this;
     }
 
@@ -94,7 +110,7 @@ public class Luban2 {
      * 图片的清晰度 内存策略
      * 优先级 ARGB_8888 >RGB_565>ARGB_4444
      */
-    public Luban2 setConfig(Bitmap.Config compressConfig) {
+    public Luban2 compressConfig(Bitmap.Config compressConfig) {
         this.compressConfig = compressConfig;
         return this;
     }
@@ -161,7 +177,8 @@ public class Luban2 {
 
     //同步
     private File synExecute() throws IOException {
-        return new CompressEngine(mFile, getImageCacheFile(), compressFormat, compressConfig).compress();
+        return new CompressEngine(mFile, getImageCacheFile())
+                .compress(compress4Sample, compressFormat, compressConfig);
     }
 
     //异步
@@ -180,26 +197,16 @@ public class Luban2 {
             return;
         }
 
-        CompressWorker worker = new CompressWorker(mFile, 0, 0, 0, listener);
+        Luban2.CompressWorker worker = new Luban2.CompressWorker(mFile, 0, listener);
         listener.onStart();
         executor.execute(worker);
     }
 
     /**
-     * @param maxSize  KB单位
-     * @param listener
+     * @param maxSize  最大大小 必须指定 否则  {@use execute(...listener)}
+     * @param listener 回调
      */
     public void executeCustom(int maxSize, OnCompressListener listener) {
-        executeCustom(maxSize, 0, 0, listener);
-    }
-
-    /**
-     * @param maxSize   最大大小 必须指定 否则  {@use execute(...listener)}
-     * @param maxWidth  最大宽度
-     * @param maxHeight 最大高度
-     * @param listener  回调
-     */
-    public void executeCustom(int maxSize, int maxWidth, int maxHeight, OnCompressListener listener) {
         if (listener == null) {
             throw new NullPointerException("the listener must be attached!");
         }
@@ -217,7 +224,7 @@ public class Luban2 {
         if (maxSize <= 0) {
             throw new NullPointerException("fuck bitch! Are you reader the method's doc?");
         }
-        CompressWorker worker = new CompressWorker(mFile, maxSize, maxWidth, maxHeight, listener);
+        Luban2.CompressWorker worker = new Luban2.CompressWorker(mFile, maxSize, listener);
         listener.onStart();
         executor.execute(worker);
     }
@@ -235,7 +242,7 @@ public class Luban2 {
             throw new NullPointerException("the listener must be attached!");
         }
 
-        MultiCompressWorker worker = new MultiCompressWorker(mFiles, listener);
+        Luban2.MultiCompressWorker worker = new Luban2.MultiCompressWorker(mFiles, listener);
         listener.onStart();
         executor.execute(worker);
     }
@@ -270,13 +277,11 @@ public class Luban2 {
 
         private File mOrignal;
         private OnCompressListener listener;
+        private int maxSize;
 
-        private int maxSize, maxWidth, maxHeight;
 
-        public CompressWorker(File file, int maxSize, int maxWidth, int maxHeight, OnCompressListener l) {
+        public CompressWorker(File file, int maxSize, OnCompressListener l) {
             this.maxSize = maxSize;
-            this.maxWidth = maxWidth;
-            this.maxHeight = maxHeight;
             this.mOrignal = file;
             this.listener = l;
         }
@@ -288,13 +293,14 @@ public class Luban2 {
                 //必须指定最大尺寸
                 if (maxSize <= 0) {
                     //单文件普通
-                    res = new CompressEngine(mOrignal, getImageCacheFile(), compressFormat, compressConfig).compress();
+                    res = new CompressEngine(mOrignal, getImageCacheFile())
+                            .compress(compress4Sample, compressFormat, compressConfig);
                 } else {
                     //自定义压缩
-                    res = new CompressEngine(mOrignal, getImageCacheFile(), compressFormat, compressConfig)
-                            .customCompress(maxSize, maxWidth, maxHeight);
+                    res = new CompressEngine(mOrignal, getImageCacheFile())
+                            .customCompress(maxSize, compress4Sample, bestQuality, compressFormat, compressConfig);
                 }
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
                 listener.onError(e);
             }
@@ -325,7 +331,8 @@ public class Luban2 {
             for (int i = 0; i < files.size(); i++) {
                 File res = files.get(i);
                 try {
-                    outPuts.add(new CompressEngine(res, getImageCacheFile(), compressFormat, compressConfig).compress());
+                    outPuts.add(new CompressEngine(res, getImageCacheFile())
+                            .compress(compress4Sample, compressFormat, compressConfig));
                 } catch (Exception ex) {
                     //失败多次回调
                     listener.onError(ex, res);
